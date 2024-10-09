33d0739997afc3d99c8ead491b4fba50d6d19467
listendown <121012615+listendown@users.noreply.github.com>
2024-10-09 01:48:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     let slideIndex = 0;
-    const slides = document.querySelectorAll('.slides img');
+    const slides = document.querySelectorAll('.mySlides');
     const showSlides = () => {
         slides.forEach((slide, index) => {
             slide.style.display = (index === slideIndex) ? 'block' : 'none';
@@ -10,3 +10,13 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     showSlides();
 });
+
+function plusSlides(n) {
+    let slides = document.querySelectorAll('.mySlides');
+    slideIndex += n;
+    if (slideIndex >= slides.length) { slideIndex = 0; }
+    if (slideIndex < 0) { slideIndex = slides.length - 1; }
+    slides.forEach((slide, index) => {
+        slide.style.display = (index === slideIndex) ? 'block' : 'none';
+    });
+}
